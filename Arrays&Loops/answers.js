7d31fe7460fbaa5f4aef6224bd65ac7e3046e3b4
JudeAshitey <110950534+JudeAshitey@users.noreply.github.com>
2022-09-14 19:36:19 -0400

    Create answers.js

---
+++ Arrays&Loops/answers.js
@@ -0,0 +1,136 @@
+////////////////////////////////
+// Easy Going
+////////////////////////////////
+// for (let i = 1; i < 21; i++) {
+//    console.log(i);
+
+// }
+//Easy Going answered
+////////////////////////////////
+// Get Even
+////////////////////////////////
+// for (let i = 0; i < 201; i++) {
+//   if (i%2 === 0) {
+//     console.log(i);
+//   }
+
+// }
+// Get Even answered
+////////////////////////////////
+// Fizz Buzz
+////////////////////////////////
+// for (let i = 0; i < 101; i++) {
+//    if (i % 3 === 0 && i % 5 === 0) {
+//       console.log("FizzBuzz");
+//    }else if (i % 3 === 0){
+//       console.log('Fizz');
+//    }else if (i % 5 === 0){
+//       console.log('Buzz');
+//    }else{
+//       console.log(i)
+//    }
+
+// }
+//FizzBuzz answered
+////////////////////////////////
+// Wild Wild Life
+////////////////////////////////
+// const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"];
+// const sharky = ["Sharky", "shark", 20, "Left Coast"];
+// const plantee = ["Plantee", "plant", 5000, "Mordor"];
+// const porgee = ["Porgee", "Porg", 186, "Ahch-To"];
+// const dart = ["D'Art", "Demogorgan Dog", 2, "Upside Down"];
+// plantee[2] = 5001;
+// wolfy[3] = "Gotham City";
+// dart.push("Hawkins");
+// wolfy[0] = "Gameboy";
+// console.log(wolfy);
+
+// Wild Wild Life answered
+
+////////////////////////////////
+// Yell at the Ninja Turtles
+////////////////////////////////
+// const NinjaT =['Donatello', 'Leonardo', 'Raphael', 'Michaelangelo'];
+
+//    for (let i of NinjaT){
+
+//     console.log(i.toUpperCase());
+//    }
+// Yell at the Ninja Turtles answered
+////////////////////////////////
+// Methods, Revisited
+////////////////////////////////
+// const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
+
+// console.log(favMovies.indexOf('Titanic'));
+
+// favMovies.sort();
+// console.log(favMovies);
+
+// favMovies.pop();
+// console.log(favMovies);
+
+// favMovies.push("Guardians of the Galaxy");
+// console.log(favMovies);
+
+// favMovies.reverse();
+// console.log(favMovies);
+
+// favMovies.shift();
+// console.log(favMovies);
+
+// favMovies.unshift();
+// console.log(favMovies);
+
+// favMovies.splice(14,1,"Avatar");
+// console.log(favMovies);
+
+// console.log(favMovies.slice(10));
+
+// let favMovies2 = favMovies.slice(10);
+// console.log(favMovies2);
+
+// console.log(favMovies.indexOf("Fast and Furious"));
+// "Methods Revisited answered"
+
+////////////////////////////////
+// Where is Waldo
+////////////////////////////////
+// const whereIsWaldo = [["Timmy", "Frank"], "Eggbert",
+//                     ["Lucinda", "Jacc", "Neff", "Snoop"],
+//                     ["Petunia", ["Baked Goods", "Waldo"]]];
+
+// whereIsWaldo.splice(1,1)
+// console.log(whereIsWaldo);
+
+// whereIsWaldo[1][2] = "No One";
+// console.log(whereIsWaldo);
+
+// console.log(whereIsWaldo[2][1][1]);
+
+//"Where is Waldo answered"
+////////////////////////////////
+//  Excited Kitten00
+// const TK = ["...human...why you taking pictures of me?...", "...the catnip made me do it...",  "...why does the red dot always get away..."]
+
+// for (let i = 0; i <20; i++) {
+//   let meow = Math.floor(Math.random()*TK.length)
+//     if (i % 2 === 0) {
+//       console.log(TK[meow])
+//     }else{
+//       console.log( "Love me, pet me! HSSSSSS!" );
+//     }
+
+// }
+////////////////////////////////
+//"Excited Kittens answered"
+////////////////////////////////
+//  Find the Median
+////////////////////////////////
+// const nums = [14,11,16,15,13,16,15,17,19,11,12,14,19,11,15,17,11,18,12,17,12,71,18,15,12];
+//  nums.sort((a,b)=>a-b)
+//  console.log(nums)
+
+// let mid = Math.floor(nums.length/2)
+// console.log(nums[mid]);
